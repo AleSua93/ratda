4ab58e90319489fb6010d6a1265fd8911d114bcc
Alejandro Suarez <alejandronicolassuarez@hotmail.com>
2022-08-06 18:26:52 -0300

    better layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <title>RATDA</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="grid h-full w-full grid-cols-4 grid-rows-4">
+      <div className="grid h-full w-full grid-cols-4 grid-rows-4 border-2 border-gray-900 p-4">
         {children}
       </div>
     </>
