146e67a4a70f3a58abd453d6a65265121458ae02
Alejandro Suarez <alejandronicolassuarez@hotmail.com>
2022-08-25 23:48:23 -0300

    initial impl

--- hooks/useAudioFiles.ts
+++ hooks/useAudioFiles.ts
@@ -1,10 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import JSZip, { JSZipObject } from "jszip";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AudioFilesDownloadUrl } from "../pages/api/v2/audio-files";
 
+export interface AudioStem {
+  id: string;
+  name: string;
+  audioElement: HTMLAudioElement;
+  active: boolean;
+}
+
+export type AudioTrack = {
+  id: string;
+  name: string;
+  stems: AudioStem[];
+};
+
 export function useAudioFiles() {
-  const [files, setFiles] = useState<any[]>([]);
+  const [tracks, setTracks] = useState<AudioTrack[]>([]);
   const { data: audioFilesDownloadUrl } = useQuery<AudioFilesDownloadUrl>(
     ["audio-files-dl-url"],
     async () => {
@@ -14,10 +27,24 @@ export function useAudioFiles() {
     }
   );
 
+  const { data: audioFiles, isLoading } = useQuery<any>(
+    ["downloaded-audio-files"],
+    async () => {
+      if (audioFilesDownloadUrl) {
+        const response = await fetch(audioFilesDownloadUrl);
+        const data = await response.blob();
+        return data;
+      }
+    },
+    { enabled: audioFilesDownloadUrl !== undefined }
+  );
+
   const parseDownloadedFiles = async (zippedFiles: {
     [key: string]: JSZipObject;
-  }) => {
+  }): Promise<AudioTrack[]> => {
     const keys = Object.keys(zippedFiles);
+
+    const res = [] as AudioTrack[];
 
     for (const key of keys) {
       const file = zippedFiles[key];
@@ -25,31 +52,44 @@ export function useAudioFiles() {
         continue;
       }
 
-      const blob = await file.async("blob");
-      console.log(file.name, blob);
+      const trackName = file.name.split("/")[0];
+      const stemName = file.name.split("/")[1].split(".mp3")[0];
+      const data = await (await file.async("blob")).text();
+      const audioElement = new Audio(data);
+
+      const trackIndex = res.findIndex((t) => t.id === trackName);
+      if (trackIndex === -1) {
+        res.push({
+          id: trackName,
+          name: trackName,
+          stems: [{ id: stemName, name: stemName, audioElement, active: true }],
+        });
+      } else {
+        res[trackIndex].stems.push({
+          id: stemName,
+          name: stemName,
+          audioElement,
+          active: true,
+        });
+      }
     }
+
+    return res;
   };
 
-  const { data: audioFiles, isLoading } = useQuery<any>(
-    ["downloaded-audio-files"],
-    async () => {
-      if (audioFilesDownloadUrl) {
-        const response = await fetch(audioFilesDownloadUrl);
-        const data = await response.blob();
-
-        const newZip = new JSZip();
-        newZip.loadAsync(data).then(async (zip) => {
-          const parsedFiles = await parseDownloadedFiles(zip.files);
-        });
-
-        return {};
-      }
-    },
-    { enabled: audioFilesDownloadUrl !== undefined }
-  );
+  useEffect(() => {
+    if (audioFiles) {
+      const newZip = new JSZip();
+      newZip.loadAsync(audioFiles).then(async (zip) => {
+        const trackData = await parseDownloadedFiles(zip.files);
+        setTracks(trackData);
+      });
+    }
+  }, [audioFiles]);
 
   return {
-    files,
+    tracks,
+    setTracks,
     isLoading,
   };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import Controls from "../components/Controls";
 import Spinner from "../components/Spinner";
 import TrackView from "../components/TrackView";
 import { AppAudioContext } from "../context/app-audio-context";
 import { useAudioFiles } from "../hooks/useAudioFiles";
-import { TrackMetadata } from "./api/audio-files";
-
-export interface AudioStem {
-  id: string;
-  audioElement: HTMLAudioElement;
-  filePath: string;
-  active: boolean;
-}
-
-export interface AudioTrack {
-  name: string;
-  id: string;
-  stems: AudioStem[];
-}
 
 const Home: NextPage = () => {
   const { audioContext, setAudioContext } = useContext(AppAudioContext);
-  const [tracks, setTracks] = useState<AudioTrack[]>([]);
-  const {
-    data: trackMetadata,
-    isLoading,
-    error,
-  } = useQuery<TrackMetadata[]>(["audio-files"], async () => {
-    const response = await fetch("/api/audio-files");
-    const data = await response.json();
-    return data;
-  });
-
-  const { files, isLoading: isDownloadingFiles } = useAudioFiles();
-
-  useEffect(() => {
-    if (!trackMetadata) {
-      return;
-    }
-
-    const tracks: AudioTrack[] = trackMetadata.map((trackData) => {
-      const prefix = "audio_files";
-
-      const stems = trackData.stems.map((stem, idx) => {
-        const filePath = `${prefix}/${trackData.name}/${stem.name}`;
-        const audioElement = new Audio(filePath);
-        return {
-          id: stem.id,
-          filePath,
-          audioElement,
-          active: idx === 0,
-        };
-      });
-
-      return {
-        id: trackData.id,
-        name: trackData.name,
-        stems,
-      };
-    });
-
-    setTracks(tracks);
-  }, [trackMetadata]);
+  const { tracks, setTracks, isLoading } = useAudioFiles();
 
   const handlePlay = () => {
     let ctx = audioContext ?? new AudioContext();
@@ -94,7 +39,7 @@ const Home: NextPage = () => {
     return tracks.find((t) => t.id === trackId)?.stems.find((s) => s.active);
   };
 
-  if (isDownloadingFiles) {
+  if (isLoading) {
     return (
       <div className="flex h-full items-center justify-center">
         <Spinner className="text-gray-500 h-8 w-8" />
